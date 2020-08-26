240076401daaf2fb0ccf437e2276d635840dbd35
Oriana <oriana.nohemi22@gmail.com>
2020-08-26 12:02:03 -0500

    prueba recibir un archivo y leerlo por consola

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const { dirname } = require('path');
+
+const enter = (ruta, callback) => {
+  fs.readFile(ruta, (err, data) => {
+    callback(data.toString())
+  })
+}
+
+enter(__dirname + '/README.md', console.log)
