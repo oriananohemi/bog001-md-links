df2a971027a0f56982182f5c051059ee450e66da
Oriana <oriana.nohemi22@gmail.com>
2020-08-26 17:37:42 -0500

    verificar que el archivo sea tipo markdown, construir ruta

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const fs = require('fs');
-const { dirname } = require('path');
+// import mdLinks from './src/md-links';
 
-const enter = (ruta, callback) => {
+const fs = require('fs');
+// const { dirname } = require('path');
+const filePath = process.argv[2];
+
+const receiveFile = (ruta, callback) => {
+
   fs.readFile(ruta, (err, data) => {
     callback(data.toString())
-  })
+    })
+}
+const checkFileType = () => {
+  const fileType = filePath.split('.').reverse()[0];
+  if(fileType === 'md') {
+    receiveFile(__dirname + '/' + filePath, console.log)
+    console.log(process)
+  } else {
+    console.log('El archivo no es tipo Markdown')
+  }
 }
 
-enter(__dirname + '/README.md', console.log)
+checkFileType()
