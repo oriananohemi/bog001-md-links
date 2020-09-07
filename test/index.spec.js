5eaff113932b540afea91d1f8cba80866897633b
Oriana <oriana.nohemi22@gmail.com>
2020-09-06 23:34:17 -0500

    test

---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+const init = require('../index');
+
+describe('init', () => {
+  it('Debe ser una funcion', () => {
+    expect(typeof init).toBe('function');
+  });
+
+  it('Debe ejecutar fileExits al cargar', () => {
+
+    })
+
+  it('Debe ejecutar mdlinks si recibe un archivo', () => {
+
+  })
+  });
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,7 +3,6 @@ const {mockMdLinks, mockMdLinksValidate, mockStats, mockStatsValidate, mockHref,
 const prueba = `${__dirname}/mock/folderMock/prueba.md`;
 
 describe('statsLinks', () => {
-
   it('Debe ser una funcion', () => {
     expect(typeof mdLinks.statsLinks).toBe('function');
   });
@@ -11,12 +10,9 @@ describe('statsLinks', () => {
   it('Si se recibe el parametro stats vamos a retornar un objeto con las key (total, unique)', () => {
     expect(mdLinks.statsLinks(mockMdLinks)).toEqual(mockStats);
   });
-});
 
-describe('mdLinks', () => {
-
-  it('Si se recibe el parametro stats vamos a retornar un objeto con las key (total, unique)', () => {
-    expect(mdLinks.mdLinks(prueba, {stats: true})).resolves.toEqual(mockStats);
+  it('Si se recibe los parametros stats y validate vamos a retornar un objeto con las key (total, unique y broke)', () => {
+    expect(mdLinks.statsLinks(mockMdLinksValidate, {validate: true})).toEqual(mockStatsValidate);
   });
 });
 
@@ -24,16 +20,14 @@ describe('validateLink', () => {
   it('Debe ser una funcion', () => {
     expect(typeof mdLinks.validateLink).toBe('function');
   });
-  it('Si se recibe el parametro validate vamos a retornar un arreglo de objetos con la url, el texto y el archivo donde se encuentra el enlace', () => {
-    // expect.assettions(1)
-    console.log(mockHref)
+
+  it('Si se recibe el parametro validate retorna una promesa con el codigo de respuesta', () => {
     expect(mdLinks.validateLink(mockHref)).resolves.toEqual(mockValidate);
   });
-});
-
-describe('validate como parametro de mdlinks', () => {
-  it('Si se recibe el parametro validate vamos a retornar un arreglo de objetos con la url, el texto y el archivo donde se encuentra el enlace', () => {
-    expect(mdLinks.mdLinks(prueba, {validate: true})).resolves.toEqual(mockMdLinksValidate);
+  it('Si se recibe el parametro validate y el archivo es un enlace interno la promesa retornara el string Es un enlace interno', () => {
+    const fileInt = '#1-resumen-del-proyecto';
+    const value = 'Es un enlace interno';
+    expect(mdLinks.validateLink(fileInt)).resolves.toEqual(value);
   });
 });
 
@@ -43,14 +37,20 @@ describe('getLinks', () => {
   });
 });
 
-describe('validate y stats como parametro de mdlinks', () => {
-  it('Si se recibe los parametros stats y validate vamos a retornar un objeto con las key (total, unique y broke)', () => {
-    expect(mdLinks.mdLinks(prueba, {stats: true, validate: true})).resolves.toEqual(mockStatsValidate);
-  });
-});
-
-describe('mdLinks', () => {
+describe('mdlinks', () => {
   it('Debe ser una funcion', () => {
     expect(typeof mdLinks.mdLinks).toBe('function');
   });
+
+  it('Si se recibe los parametros stats y validate vamos a retornar un objeto con las key (total, unique y broke)', () => {
+    expect(mdLinks.mdLinks(prueba, {stats: true, validate: true})).resolves.toEqual(mockStatsValidate);
+  });
+
+  it('Si se recibe el parametro stats vamos a retornar un objeto con las key (total, unique)', () => {
+    expect(mdLinks.mdLinks(prueba, {stats: true})).resolves.toEqual(mockStats);
+  });
 });
+
+it('Si se recibe el parametro validate vamos a retornar un arreglo de objetos con la url, el texto y el archivo donde se encuentra el enlace', () => {
+    expect(mdLinks.mdLinks(prueba, {validate: true})).resolves.toEqual(mockMdLinksValidate);
+  });
