ed7845a205d3705613c4d0799283976e67419bf0
Oriana <oriana.nohemi22@gmail.com>
2020-09-07 12:55:52 -0500

    actualizar libreria

--- src/md-links.js
+++ src/md-links.js
@@ -14,7 +14,7 @@ const statsLinks = (links, validate = false) => {
 
     if(validate) {
       stats.broken = linksUniqueArray.reduce((accumulator, currentElement) => {
-        accumulator += currentElement.status > 400 ? 1 : 0;
+      accumulator += currentElement.status > 399 || currentElement.status === 'ENOENT' ? 1 : 0;
         return accumulator;
       }, 0)
     }
@@ -70,7 +70,7 @@ const getLinks = (data, options, filePath) => {
       .then((result) => {
         result.forEach((res, index) => {
           links[index].status = res.value || res.reason;
-          links[index].value = res.value < 400 ? 'OK' : 'Fail'
+          links[index].value = res.value < 400 || links[index].status === 'Es un enlace interno' ? 'OK' : 'Fail'
         })
         if(options.stats) {
           const stats = statsLinks(links, options.validate)
