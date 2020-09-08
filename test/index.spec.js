85f49202c8cb10c4cf7f4e1aa8131f61acc32969
Oriana <oriana.nohemi22@gmail.com>
2020-09-07 22:33:21 -0500

    resuelvo errores

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,28 +3,7 @@ const mdLinks = require('../src/md-links');
 const utils = require('../src/utils.js');
 
 describe('init', () => {
-  xit('Debe ser una funcion', () => {
+  it('Debe ser una funcion', () => {
     expect(typeof init).toBe('function');
   });
-
-  xit('Debe retornar un error si el archivo/ruta ingresada no existe', () => {
-    jest.spyOn(utils, 'fileExists').mockImplementationOnce(() => false);
-
-    const result = init('string');
-
-    })
-
-    xit('Debe ejecutar mdlinks si recibe un archivo existente', () => {
-      jest.spyOn(utils, 'fileExists').mockImplementationOnce(() => true);
-      jest.spyOn(utils, 'isFile').mockImplementationOnce(() => true);
-  
-      init('string');
-      mdLinks.mdLinks('sting','options')
-  
-      expect(mdLinks.mdLinks).toHaveBeenCalledTimes(1)
-      })
-
-  xit('Debe ejecutar mdlinks si recibe un archivo', () => {
-
-  })
   });
